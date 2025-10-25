632806c0447b3d6f6a5b78c1741f92c161bdb3a0
Seele Volleri <166212217+SeeleVolleri@users.noreply.github.com>
2025-10-25 21:05:02 +0800

    Check whether condiitions met

--- service.sh
+++ service.sh
@@ -1,2 +1,13 @@
 #!/system/bin/sh
+
+while [ "$(getprop sys.user.0.ce_available)" != "true" ]; do
+  sleep 2
+done
+
+if [ -z "$(getprop persist.sys.ifw.enabled)" ]; then
+  exit 0
+fi
+
 [ "$(getprop persist.sys.ifw.enabled)" = "false" ] && setprop persist.sys.ifw.enabled true
+
+exit 0
